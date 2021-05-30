d89770370f29c8388f985e61a4a45fd407c7c360
ctoweyburke <ctowey21@yahoo.com>
2021-05-30 01:10:17 +0000

    index.page - change of layout - mobile responsive also completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
+//Javascript for Hamburger Menu - followed tutorial by https://marina-ferreira.github.io/tutorials/js/memory-game/
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+
 //Javascript for Memory Game - followed tutorial by https://marina-ferreira.github.io/tutorials/js/memory-game/
 
 //Memory Cards Variables
@@ -13,96 +22,96 @@ var matchCount = 0
 
 //Function set time
 function setTime() {
-  document.getElementById('timer').textContent = ++totalSeconds;
+    document.getElementById('timer').textContent = ++totalSeconds;
 }
 
 //Function START GAME
-function startGame(){
-  if(timerFunc == null){
-    timerFunc = setInterval(setTime, 1000);
-  }
-  cards.forEach(card => card.addEventListener('click', flipCard));
+function startGame() {
+    if (timerFunc == null) {
+        timerFunc = setInterval(setTime, 1000);
+    }
+    cards.forEach(card => card.addEventListener('click', flipCard));
 }
 
 //Function to RESET GAME
-function resetGame(){
-  if(timerFunc != null){
-    clearInterval(timerFunc);
-  }
-  timerFunc = null;
-  totalSeconds = 0;
-  document.getElementById('flips').textContent = 0;
-  document.getElementById('timer').textContent = 0;
-  location.reload()
+function resetGame() {
+    if (timerFunc != null) {
+        clearInterval(timerFunc);
+    }
+    timerFunc = null;
+    totalSeconds = 0;
+    document.getElementById('flips').textContent = 0;
+    document.getElementById('timer').textContent = 0;
+    location.reload()
 }
 
 //Function INCREASE FLIP COUNTER
-function increamentFlipCount(){
-  document.getElementById('flips').textContent = parseInt(document.getElementById('flips').textContent) + 1;
+function increamentFlipCount() {
+    document.getElementById('flips').textContent = parseInt(document.getElementById('flips').textContent) + 1;
 }
 
 //Function to FLIP CARDS
 function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
-  increamentFlipCount()
+    if (lockBoard) return;
+    if (this === firstCard) return;
+    increamentFlipCount()
 
-  this.classList.add('flip');
+    this.classList.add('flip');
 
-  if (!hasFlippedCard) {
-    hasFlippedCard = true;
-    firstCard = this;
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
 
-    return;
-  }
-
-  secondCard = this;
-  checkForMatch();
-  if(matchCount == 6){
-    if(timerFunc != null){
-      clearInterval(timerFunc);
+        return;
     }
-    let flips = document.getElementById('flips').textContent
-    document.getElementById('finished_messsage').textContent = "You completed the game in " + totalSeconds + " seconds using " + flips + " flips"
-  }
+
+    secondCard = this;
+    checkForMatch();
+    if (matchCount == 6) {
+        if (timerFunc != null) {
+            clearInterval(timerFunc);
+        }
+        let flips = document.getElementById('flips').textContent
+        document.getElementById('finished_messsage').textContent = "You completed the game in " + totalSeconds + " seconds using " + flips + " flips"
+    }
 }
 
 //Function CHECK FOR CARDS MATCH
 function checkForMatch() {
-  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
-  isMatch ? disableCards() : unflipCards();
+    isMatch ? disableCards() : unflipCards();
 }
 
 function disableCards() {
-  firstCard.removeEventListener('click', flipCard);
-  secondCard.removeEventListener('click', flipCard);
-  document.getElementById('matched').textContent = ++matchCount;
-  resetBoard();
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    document.getElementById('matched').textContent = ++matchCount;
+    resetBoard();
 }
 
 //Function TO UNFLIP CARDS
 function unflipCards() {
-  lockBoard = true;
+    lockBoard = true;
 
-  setTimeout(() => {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
 
-    resetBoard();
-  }, 1500);
+        resetBoard();
+    }, 1500);
 }
 
 //Function TO RESET BOARD
 function resetBoard() {
-  [hasFlippedCard, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null];
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 //Function TO SHUFFLE CARDS so they are in different locations each time
 (function shuffle() {
-  cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 12);
-    card.style.order = randomPos;
-  });
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
 })();
